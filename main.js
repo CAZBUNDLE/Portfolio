fea8e8a2215419d1e372c95061b06f81842ac2e7
CAZBUNDLE <74971704+CAZBUNDLE@users.noreply.github.com>
2021-07-20 21:02:21 +0100

    updated

--- main.js
+++ main.js
@@ -9,17 +9,68 @@
 // }
 // =================================================
 
+
 $(document).ready(function(){
+
+    // ==========drop down ===========
     $('.menu-toggler').on('click', function(){
-        $(this).togglerClass('open');
+        $(".menu-toggler").togglerClass('open');
         $('.top-nav').togglerClass('open');
     });
+
+    // ======smoother scrolling =======
+    $('.top-nav .nav-link').on('click', function(){
+        $(".menu-toggler").removeClass('open');
+        $('.top-nav').remove.Class('open');
+    });
+
+    $('nav .a[href*="#"]').on('click', function(){
+       $('html, body').animate (keyframes,{
+           scrollTop:$($(this).attr('rhref')).offset().top - 100
+       },options,2000);
+    });
+
+    // ========to-top-button=======
+
+    $('#up').on('click', function(){
+        $('html, body').animate (keyframes,{
+            scrollTop: 0
+       }, options,2000);
+    });
+
+    Aos.init({
+        easing: 'ease',
+        duration:1800
+    })
+        
 });
 
 
 
 
-
+// - First i've put the .menu-toggler and the .top-nav into a new div named with id="navigationBar".
+// - then in my script.js file, i've added this code :  
+// const nav = $("#navigationBar");
+//   const navTop = nav.offset().top;
+//   $(window).on("scroll", stickyNavigation);
+//   function stickyNavigation() {
+//       var body = $("body");
+//       if($(window).scrollTop() >= navTop) {
+//           body.css("padding-top", nav.outerHeight() + "px");
+//           body.addClass("fixedNav");
+//       } else {
+//           body.css("padding-top", 0);
+//           body.removeClass("fixedNav");
+//       }
+//   }
+// -then in my css file,  
+//  .fixedNav #navigationBar {
+//   position: fixed;
+//   top:0;
+// }
+// #navigationBar{
+//   width: 100%;
+// }
 
 
 
